d4c0a1895237a8567afa635ef11519326746fc74
Peter Nickolov <pnickolov@users.noreply.github.com>
2024-02-08 07:51:01 -0800

    improve startup file handling of env vars

--- startup.sh
+++ startup.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-export OTEL_RESOURCE_ATTRIBUTES="$OTEL_RESOURCE_ATTRIBUTES","container.id="$(sed -rn "/\/sys\/fs\/cgroup\/devices/ s#.*/(cri-containerd-)?([0-9a-f]{64})(\.scope)? .*#\2#p" /proc/self/mountinfo)
+if [[ -n "$OTEL_RESOURCE_ATTRIBUTES" ]]; then
+  OTEL_RESOURCE_ATTRIBUTES="$OTEL_RESOURCE_ATTRIBUTES,"
+fi
+export OTEL_RESOURCE_ATTRIBUTES="${OTEL_RESOURCE_ATTRIBUTES}container.id="$(sed -rn "/\/sys\/fs\/cgroup\/devices/ s#.*/(cri-containerd-)?([0-9a-f]{64})(\.scope)? .*#\2#p" /proc/self/mountinfo)
 echo "Starting 'co-http $@' with the following environment variables:"
 echo "OTEL_EXPORTER_OTLP_ENDPOINT=$OTEL_EXPORTER_OTLP_ENDPOINT"
+echo "OTEL_EXPORTER_OTLP_INSECURE=$OTEL_EXPORTER_OTLP_INSECURE"
 echo "OTEL_RESOURCE_ATTRIBUTES=$OTEL_RESOURCE_ATTRIBUTES"
+echo "OTEL_TRACES_EXPORTER=$OTEL_TRACES_EXPORTER"
+echo "OTEL_SERVICE_NAME=$OTEL_SERVICE_NAME"
 exec /co-http "$@"
